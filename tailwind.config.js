c4fed439d3e773ce073b98b03ef2bf25a1b6dd25
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2025-05-01 19:45:05 +0600

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.tsx',
+        './resources/**/*.ts',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
